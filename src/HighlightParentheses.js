a2c4fad74ff7540b0cfaefaefdb0143343053d6b
definite2 <edefne878@gmail.com>
2022-08-14 18:46:19 +0300

    add comments

--- src/HighlightParentheses.js
+++ src/HighlightParentheses.js
@@ -1,26 +1,30 @@
+/**
+ * React component which highlights paired parentheses
+ */
 import React from 'react';
 
 const HighlightParentheses = ({ text }) => {
-  const pairKeyIndexStack = [];
-  const highlightsIndexes = Array.from({ length: text.length }).map(
-    (i) => false
-  );
+  // initialize a stack for parentheses:
+  const parenthesesIndexStack = [];
+  // initialize an array with length text.length, with inital values are false:
+  const higlightedIndices = Array(text.length).fill(false);
   for (let i = 0; i < text.length; i++) {
     if (text[i] === '(') {
-      pairKeyIndexStack.push(i);
-    } else if (text[i] === ')' && pairKeyIndexStack.length !== 0) {
-      highlightsIndexes[pairKeyIndexStack.pop()] = true;
-      highlightsIndexes[i] = true;
+      parenthesesIndexStack.push(i);
+    } else if (text[i] === ')' && parenthesesIndexStack.length !== 0) {
+      // since paired parentheses are found make corresponding indices' values to true:
+      higlightedIndices[parenthesesIndexStack.pop()] = true; // opening parenthesis
+      higlightedIndices[i] = true; // closing parenthesis
     }
   }
   return (
     <>
-      {[...text].map((str, i) => (
+      {[...text].map((char, i) => (
         <>
-          {highlightsIndexes[i] ? (
-            <i className="highlight">{str}</i>
+          {higlightedIndices[i] ? (
+            <i className="highlight">{char}</i>
           ) : (
-            <>{str}</>
+            <>{char}</>
           )}
         </>
       ))}
